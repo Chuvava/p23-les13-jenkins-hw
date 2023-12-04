cfd96feba0318ea799d85b7e51897dee634c91ea
Aleksandr Efimov <aleksandr.efimov94@mail.ru>
2023-12-04 21:57:42 +0100

    added screenshot attach

--- src/test/java/com/aefimov/BaseTest.java
+++ src/test/java/com/aefimov/BaseTest.java
@@ -1,6 +1,9 @@
 package com.aefimov;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class BaseTest {
@@ -11,6 +14,12 @@ public class BaseTest {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy="eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAtachments() {
+        helpers.Attach.screenshotAs("Last screenshot");
     }
 
 }
